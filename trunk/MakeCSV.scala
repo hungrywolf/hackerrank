4092df55adcc8d635e0aacd0bdad99664d284b2a
ligadata-saleh <saleh@ligadata.com>
2017-09-14 21:44:39 +0300

    final changes

--- trunk/MakeCSV.scala
+++ trunk/MakeCSV.scala
@@ -1,10 +1,26 @@
 import java.io._
 
-val data = List("everything", "you", "want", "to", "write", "to", "the", "file")
+val file = "/tmp/rejecteddata.csv"
+val writer = new PrintWriter(new File(file))
+for(i <- 0 to 1000)
+  writer.write( List("AARCNT1002858","/home/bigdata/applications/group/intec_int/kprod/incoming/"+i,"144865","20170909 00:11:40","20170909",""+i%7,""+i%5,"reson "+i%5,"20170829043941-0400").map(e=>e).mkString(","))
 
-val file = "~/Desktop/whatever.txt"
-val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)))
-for (x <- data) {
-  writer.write(x + "\n")  // however you want to format it
+writer.close()
+
+
+
+val file = "/tmp/airfill.csv"
+val writer = new PrintWriter(new File(file))
+for (i <- 0 to 1000) {
+  writer.write(List("/home/bigdata/applications/group/intec_int/kprod/incoming/"+i,"2017-09-01",""+i%7,""+i%5,	"20170829043941-0400").map(e=>e).mkString(","))
+}
+writer.close()
+
+
+
+val file = "/tmp/filestats.csv"
+val writer = new PrintWriter(new File(file))
+for (i <- 0 to 1000) {
+  writer.write(List("/home/bigdata/applications/group/intec_int/kprod/incoming/"+i,""+i,	"2017-08-01",""i%23,"20170829043941").map(e=>e).mkString(","))
 }
 writer.close()
